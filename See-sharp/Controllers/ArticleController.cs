059e05d50c757d075c58d114caf3c473abe24a89
animbadger <anim.ansah.stephen@gmail.com>
2024-09-09 18:42:49 +0000

    added controllers

---
+++ See-sharp/Controllers/ArticleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using See_sharp.Data;
+using See_sharp.Models;
+
+namespace See_sharp.Controllers;
+
+[Route("API/[controller]")]
+[ApiController]
+public class ArticleController : ControllerBase
+{
+    private readonly ApiDbContext _context;
+    
+    public ArticleController(ApiDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Article>>> GetArticles()
+    {
+        return await _context.Articles.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Article>> GetAnArticle(int id)
+    {
+        var article = await _context.Articles.FindAsync(id);
+
+        if (article == null)
+        {
+            return NotFound();
+        }
+        return article;
+    }
+
+}
---
+++ See-sharp/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using See_sharp.Models;
+
+namespace See_sharp.Data;
+
+public class ApiDbContext: DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Article> Articles { get; set; }
+
+}
